54e84cc2287db2827ae482266e1a1da3f3296439
Jakub Jakubowski <kubajot6@wp.pl>
2022-06-30 07:41:49 +0100

    Complete scroll sections on touchscreen

--- js/Scroll.js
+++ js/Scroll.js
@@ -3,6 +3,7 @@ export class Scroll {
     this.sections = null;
     this.currentSectionIndex = 0;
     this.isScroll = false;
+    this.isWheel = false;
 
     this.touchStart = null;
     this.scrollDirection = null;
@@ -19,7 +20,11 @@ export class Scroll {
   }
 
   addListeners() {
-    document.addEventListener("wheel", (e) => this.setCurrentSectionIndex(e));
+    document.addEventListener("wheel", (e) => {
+      this.isWheel = true;
+      this.setCurrentSectionIndex(e);
+      this.isWheel = false;
+    });
 
     document.addEventListener(
       "touchstart",
@@ -30,7 +35,11 @@ export class Scroll {
 
   setCurrentSectionIndex(e) {
     const lengthOfSections = document.querySelectorAll(".section").length;
-    this.scrollDirection = e.wheelDelta;
+
+    this.isWheel
+      ? (this.scrollDirection = e.wheelDelta)
+      : (this.scrollDirection = this.scrollDirection);
+    // this.scrollDirection = e.wheelDelta;
 
     if (!this.isScroll) {
       this.checkIsScroll();
@@ -45,7 +54,7 @@ export class Scroll {
       this.currentSectionIndex = lengthOfSections - 1;
     }
     this.sectionOnView(this.currentSectionIndex);
-    console.log("currentsectionindex" + this.currentSectionIndex);
+    // console.log("currentsectionindex: " + this.currentSectionIndex);
   }
 
   checkIsScroll() {
@@ -60,12 +69,20 @@ export class Scroll {
   }
 
   checkTouch(e) {
+    // e.preventDefault();
     const touchPosition = e.touches[0].clientY;
-
+    console.log(
+      "this.touchStart - touchPosition: " + (this.touchStart - touchPosition)
+    );
+    // console.log(" touchPosition: " + touchPosition);
+    // console.log("tochstart: " + this.touchStart);
+    // console.log("touch position: " + touchPosition);
     this.touchStart - touchPosition > 0
-      ? (this.scrollDirection = 1)
-      : (this.scrollDirection = -1);
+      ? (this.scrollDirection = -1)
+      : (this.scrollDirection = 1);
 
     this.setCurrentSectionIndex(e);
+    console.log(this.scrollDirection);
+    this.scrollDirection = 0;
   }
 }
